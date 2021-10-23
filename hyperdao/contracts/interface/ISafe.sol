d548f6bc6e2915fab693af44bc9ac5e206253970
kin <kinspotting@protonmail.com>
2021-10-23 22:21:14 +0200

    add contracts

---
+++ hyperdao/contracts/interface/ISafe.sol
@@ -0,0 +1,23 @@
+pragma solidity 0.8.9;
+
+contract Enum {
+    enum Operation {
+        Call,
+        DelegateCall
+    }
+}
+
+interface Safe {
+    function getTransactionHash(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        Enum.Operation operation,
+        uint256 safeTxGas,
+        uint256 baseGas,
+        uint256 gasPrice,
+        address gasToken,
+        address refundReceiver,
+        uint256 _nonce
+    ) external view returns (bytes32);
+}
---
+++ hyperdao/contracts/test/Imports.sol
@@ -0,0 +1,5 @@
+// solium-disable linebreak-style
+pragma solidity 0.8.6;
+
+import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
